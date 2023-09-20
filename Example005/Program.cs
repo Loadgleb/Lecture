fcc5ff343d34b8cf54092525e6fe9ffb5f807cdc
Gleb <eg1994@rambler.ru>
2023-09-20 13:42:09 +0300

    Ex5

---
+++ Example005/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Wrate("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Глеб");
+ {Console.WriteLine("Ура, это же ГЛЕБ! ");}
+else
+{Console.Write("Привет, ");
+Console.WriteLine(username);}
